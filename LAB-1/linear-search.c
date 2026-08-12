6aba22626011d20e84e36b83668c08513298c681
Manepally Harshit <harshitmanepally1524@gmail.com>
2026-08-12 10:37:47 +0530

    Add files via upload

--- LAB-1/linear-search.c
+++ LAB-1/linear-search.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main() {
   int n, key, i;
+
   printf("Enter the size of the array: ");
   scanf("%d", &n);
 
   int arr[n];
-  printf("EEnter the values of the array: ");
-  for(int i = 0; i < n; i++){
+  printf("Enter the values of the array: ");
+  for (i = 0; i < n; i++) {
     scanf("%d", &arr[i]);
   }
 
-  printf("EEnter the value whose index you want to find in the array: ");
+  printf("Enter the value whose index you want to find in the array: ");
   scanf("%d", &key);
-  for(i = 0; i < n; i++){
-    if(key == arr[i]){
-      printf("THe index of the value in the array is %d", i);
+  for (i = 0; i < n; i++) {
+    if (key == arr[i]) {
+      printf("The index of the value in the array is %d.\n", i);
       return 0;
     }
   }
-  printf("THe elem is not in the array.");
+
+  printf("The element is not in the array.\n");
   return 0;
 }
